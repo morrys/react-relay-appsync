f2981a4e08e81e815faaff88d6bae11aa92072a7
morrys <lorenzo.digiacomo85@gmail.com>
2020-03-15 11:18:52 +0100

    fix Accept & Content-Type headers

--- packages/react-relay-auth-iam/src/middleware-auth-iam.ts
+++ packages/react-relay-auth-iam/src/middleware-auth-iam.ts
@@ -1,28 +1,45 @@
-import { Middleware, RelayNetworkLayerRequest } from 'react-relay-network-modern/lib/definition';
-import { Credentials, CredentialsOptions } from 'aws-sdk/lib/credentials';
-import { Signer } from './signer';
-import * as Url from 'url';
-const SERVICE = 'appsync';
+import {
+  Middleware,
+  RelayNetworkLayerRequest
+} from "react-relay-network-modern/lib/definition";
+import { Credentials, CredentialsOptions } from "aws-sdk/lib/credentials";
+import { Signer } from "./signer";
+import * as Url from "url";
+const SERVICE = "appsync";
 
-type CredentialsGetter = () => (Credentials | CredentialsOptions | Promise<Credentials> | Promise<CredentialsOptions> | null) | Credentials | CredentialsOptions | Promise<Credentials> | Promise<CredentialsOptions> | null;
+type CredentialsGetter = () =>
+  | (
+      | Credentials
+      | CredentialsOptions
+      | Promise<Credentials>
+      | Promise<CredentialsOptions>
+      | null
+    )
+  | Credentials
+  | CredentialsOptions
+  | Promise<Credentials>
+  | Promise<CredentialsOptions>
+  | null;
 
 export type AuthIAMMiddlewareOpts = {
-  credentials: CredentialsGetter
-  region: string
+  credentials: CredentialsGetter;
+  region: string;
 };
 
-export default function authIAMMiddleware(opts: AuthIAMMiddlewareOpts): Middleware {
-  const {
-    credentials,
-    region
-  } = opts;
-  return (next) => async (req: RelayNetworkLayerRequest) => {
+export default function authIAMMiddleware(
+  opts: AuthIAMMiddlewareOpts
+): Middleware {
+  const { credentials, region } = opts;
+  return next => async (req: RelayNetworkLayerRequest) => {
     const service = SERVICE;
     const url = req.fetchOpts.url;
 
-    let creds = typeof credentials === 'function' ? (credentials as any).call() : (credentials || {});
+    let creds =
+      typeof credentials === "function"
+        ? (credentials as any).call()
+        : credentials || {};
 
-    if (creds && typeof creds.getPromise === 'function') {
+    if (creds && typeof creds.getPromise === "function") {
       await creds.getPromise();
     }
 
@@ -32,28 +49,35 @@ export default function authIAMMiddleware(opts: AuthIAMMiddlewareOpts): Middlewa
 
     const formatted = {
       ...formatAsRequest(req),
-      service, region, url, host, path
+      service,
+      region,
+      url,
+      host,
+      path
     };
 
-    const { headers } = Signer.sign(formatted, { access_key: accessKeyId, secret_key: secretAccessKey, session_token: sessionToken });
+    const { headers } = Signer.sign(formatted, {
+      access_key: accessKeyId,
+      secret_key: secretAccessKey,
+      session_token: sessionToken
+    });
     req.fetchOpts.headers = {
       ...req.fetchOpts.headers,
       ...headers
-    }
+    };
     return next(req);
   };
-
-
 }
 
+// Accept Content-Type
 const formatAsRequest = ({ fetchOpts }) => {
   return {
     body: fetchOpts.body,
     method: fetchOpts.method,
     headers: {
-      accept: '*/*',
-      'content-type': 'application/json; charset=UTF-8',
-      ...fetchOpts.headers,
-    },
+      Accept: "*/*",
+      "Content-Type": "application/json; charset=UTF-8",
+      ...fetchOpts.headers
+    }
   };
-}
+};
